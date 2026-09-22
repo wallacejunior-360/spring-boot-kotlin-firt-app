6c211d20798bd2a54e5a72c5e0e7b34c2dc26cc6
wallacejunior-360 <wallace.junior@meuvarejo360.com.br>
2026-09-22 12:53:47 -0300

    Fix: add new math functions

--- spring-boot-kotlin-firt-app/src/main/kotlin/br/com/wallace360/MathController.kt
+++ spring-boot-kotlin-firt-app/src/main/kotlin/br/com/wallace360/MathController.kt
@@ -35,15 +35,59 @@ class MathController {
         @PathVariable(value = "numberTwo") numberTwo: String?
     ): Double {
 
-        // Tenta converter ambos para Double
         val first = numberOne?.toDoubleOrNull()
         val second = numberTwo?.toDoubleOrNull()
 
-        // Se qualquer um for nulo (ou seja, não era um número válido), lança a sua exceção
         if (first == null || second == null) {
             throw UnsupportedMathOperationException("Please set a numeric value")
         }
 
         return first - second
+    }
+
+    @RequestMapping(value = ["/div/{numberOne}/{numberTwo}"])
+    fun div(
+        @PathVariable(value = "numberOne") numberOne: String?,
+        @PathVariable(value = "numberTwo") numberTwo: String?
+    ): Double {
+
+        val first = numberOne?.toDoubleOrNull()
+        val second = numberTwo?.toDoubleOrNull()
+
+        if (first == null || second == null) {
+            throw UnsupportedMathOperationException("Please set a numeric value")
+        }
+
+        return first / second
+    }
+
+    @RequestMapping(value = ["/mul/{numberOne}/{numberTwo}"])
+    fun mul(
+        @PathVariable(value = "numberOne") numberOne: String?,
+        @PathVariable(value = "numberTwo") numberTwo: String?
+    ): Double {
+
+        val first = numberOne?.toDoubleOrNull()
+        val second = numberTwo?.toDoubleOrNull()
+
+        if (first == null || second == null) {
+            throw UnsupportedMathOperationException("Please set a numeric value")
+        }
+
+        return first * second
+    }
+
+    @RequestMapping(value = ["/sqrt/{numberOne}"])
+    fun sqrt(
+        @PathVariable(value = "numberOne") numberOne: String?
+    ): Double {
+
+        val first = numberOne?.toDoubleOrNull()
+
+        if (first == null) {
+            throw UnsupportedMathOperationException("Please set a numeric value")
+        }
+
+        return kotlin.math.sqrt(first)
     }
 }
